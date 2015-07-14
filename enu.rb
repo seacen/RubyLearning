9fd8198a673c38baefc271a3be32676af4619ed9
Xichang(Seacen) Zhao <xichangzhao@outlook.com>
2015-07-14 19:56:50 +1000

    proc block

--- enu.rb
+++ enu.rb
@@ -1,12 +1,22 @@
 # written by seacen
 
-def Integer.all
-  Enumerator.new do |yielder, n: 0|
-    loop { yielder.yield(n += 1) }
-  end.lazy
+def my_if(condition, then_clause, else_clause)
+  if condition
+    then_clause.call
+  else
+    else_clause.call
+  end
 end
-
-p Integer.all.select { |i| (i % 3).zero? }.first(10)
+5.times do |val|
+  my_if(val < 2, -> { puts "#{val} is small" }, -> { puts "#{val} is big" })
+end
+# def Integer.all
+#   Enumerator.new do |yielder, n: 0|
+#     loop { yielder.yield(n += 1) }
+#   end.lazy
+# end
+#
+# p Integer.all.select { |i| (i % 3).zero? }.first(10)
 
 # triangular_numbers = Enumerator.new do |yielder|
 #   number = 0
@@ -16,7 +26,7 @@ p Integer.all.select { |i| (i % 3).zero? }.first(10)
 #     count += 1
 #     yielder.yield number
 #   end
-# end
+# end.lazy
 #
 # def infinite_select(enum)
 #   Enumerator.new do |yielder|
@@ -26,4 +36,4 @@ p Integer.all.select { |i| (i % 3).zero? }.first(10)
 #   end
 # end
 #
-# p infinite_select(triangular_numbers) { |val| val % 7 == 0 }.first(5)
+# p triangular_numbers.select { |val| val % 7 == 0 }
