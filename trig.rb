acca2af0c1991ec806d5843a69a10f98661f54b3
Xichang(Seacen) Zhao <xichangzhao@outlook.com>
2015-07-18 19:25:05 +1000

    using scope resolution operator

--- trig.rb
+++ trig.rb
@@ -1,11 +1,4 @@
-#---
-# Excerpted from "Programming Ruby 1.9 and 2.0",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/ruby4 for more book information.
-#---
+# trigometry
 module Trig
   PI = 3.141592654
   def self.sin(x)
@@ -15,10 +8,6 @@ module Trig
   def self.cos(x)
     puts x
   end
-
-  def self.pi
-    PI
-  end
 end
 
-Trig.cos(Trig.pi)
+Trig.cos(Trig::PI)
