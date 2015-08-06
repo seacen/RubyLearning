4cf9937689d59d6e0ef0a854af0f051c2253cd63
Xichang(Seacen) Zhao <xichangzhao@outlook.com>
2015-08-07 02:19:14 +1000

    ex3

--- workshops/ex2.rb
+++ workshops/ex2.rb
@@ -20,7 +20,24 @@ def average(array)
   array.inject(:+).to_f / array.size
 end
 
+def sum_binary(array)
+  array.inject(:+).to_s(2)
+end
+
+def flatten_array(array)
+  new_a = []
+  array.each do |a|
+    a.each do |i|
+      new_a << i
+    end
+  end
+  new_a
+end
+
 # test:
+# aoa=[['c','f','g'],[2,5,7],[34,56,67,89]]
+# p flatten_array(aoa)
 # array=[1,2,3,4]
+# puts sum_binary(array)
 # puts average(array)
 # puts odd_even_array(array)
---
+++ workshops/ex3.rb
@@ -0,0 +1,26 @@
+# Exercise 3
+class DiceRoll
+  attr_reader :num, :sides
+  def initialize(num, sides)
+    @num = num
+    @sides = sides
+  end
+
+  def all_possibilities
+    (1..@sides).to_a.repeated_combination(@num) { |c| p c }
+  end
+
+  def roll_dice
+    prng = Random.new
+    result = []
+    @num.times do
+      result << prng.rand(1..@sides)
+    end
+    p result
+  end
+end
+
+# test:
+# dice = DiceRoll.new(3, 7)
+# dice.roll_dice
+# dice.all_possibilities
